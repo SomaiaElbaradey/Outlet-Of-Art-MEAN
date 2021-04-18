1d841783c1ba637fa7ca10c353cb7b9ce59221b6
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-04-18 05:24:05 +0200

    dist folder

--- routes/user.js
+++ routes/user.js
@@ -1,154 +1,146 @@
-const bcrypt = require("bcrypt");
-const _ = require("lodash");
-const express = require("express");
+const bcrypt = require('bcrypt')
+const _ = require('lodash');
+const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require("express-validator");
-const User = require("../schema/user");
-const auth = require("../middleware/auth");
-const upload = require("../middleware/upload");
-const { sendMail } = require("../helpers/verifyMail");
-const { forEach } = require("underscore");
+const { body, validationResult } = require('express-validator');
+const User = require('../schema/user');
+const auth = require('../middleware/auth')
+const upload = require('../middleware/upload')
+const { sendMail } = require('../helpers/verifyMail');
+const { forEach } = require('underscore');
 let imageUrl;
-const jwt=require('jsonwebtoken')
 
-//// register new user
-router.post(
-  "/register",
-  body("email").isLength({ min: 1 }).withMessage("email is required"),
-  body("username").isLength({ min: 1 }).withMessage("username is required"),
-  body("password").isLength({ min: 4 }).withMessage("password is required"),
-  async (req, res) => {
+//// register new user 
+router.post('/register'
+  , body('email').isLength({ min: 1 })
+    .withMessage('email is required'),
+  body('username').isLength({ min: 1 })
+    .withMessage('username is required'),
+  body('password').isLength({ min: 4 })
+    .withMessage('password is required')
+  , async (req, res) => {
     ///// body validation
 
     const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(400).send({ error: errors.errors[0].msg });
+    if (!errors.isEmpty()) return res.status(400).send({ error: errors.errors[0].msg });
 
     ////// chech if user register before
-    let isUser = await User.findOne({ username: req.body.username });
-    if (isUser) return res.status(400).send("unvalid username");
-    isUser = await User.findOne({ email: req.body.email });
-    if (isUser) return res.status(400).send("user already registered");
-    ///////////////check if image uploaded
+    let isUser = await User.findOne({ username: req.body.username })
+    if (isUser) return res.status(400).send('unvalid username')
+    isUser = await User.findOne({ email: req.body.email })
+    if (isUser) return res.status(400).send('user already registered')
+    ///////////////check if image uploaded 
 
     ////// create new user
     const user = new User({
       email: req.body.email,
       username: req.body.username,
       password: req.body.password,
+      //// image:image.url,
       gender: req.body.gender,
-      isAdmin: req.body.isAdmin,
+      isAdmin: req.body.isAdmin
+
     });
     //// hashing password
     const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    user.password = await bcrypt.hash(user.password, salt)
 
     ///// save new user
     try {
-      await user.save();
+      await user.save()
       sendMail(user.email, user.username, user._id);
-      /////////// create token by user id //////////
-      const token = jwt.sign(
-        { _id: user._id, isAdmin: user.isAdmin },
-        process.env.SECRET_KEY
-      );
-      res
-        .send({
-          message: "user was registed successfully",
-          email: user.email,
-          token: token,
-          isAdmin: user.isAdmin,
-        });
-    } catch (err) {
-      res.send({ error: err });
-    }
-  }
-);
-router.patch("/image", upload, auth, async (req, res) => {
-  ///// body validation
-  console.log(req.file);
-  const loginedID = req.user._id;
-  let updates;
-  let user = await User.findById(loginedID);
-  let _image = req.file.filename;
 
-  console.log(`${process.env.API}/${_image}`);
-
-  try {
-    if (_image) {
-      updates = { image: `${process.env.API}/${_image}` };
-    } else {
-      updates = { image: user.image };
+      return res.send({ message: 'user was registered successfully' })
     }
-    user = await User.findByIdAndUpdate(loginedID, updates, {
-      new: true,
-    });
-    if (user)
-      return res.send({
-        message: "user was edited successfully",
-        image: user.image,
+    catch (err) {
+      res.send({ error: err })
+    }
+
+  })
+router.patch('/image', upload, auth
+  , async (req, res) => {
+    ///// body validation
+    console.log(req.file)
+    const loginedID = req.user._id
+    let updates
+    let user = await User.findById(loginedID);
+    let _image = req.file.filename
+
+    console.log(`${process.env.API}/${_image}`)
+
+    try {
+      if (_image) {
+        updates =
+        { image: `${process.env.API}/${_image}` }
+      }
+      else {
+        updates =
+          { image: user.image }
+      }
+       user = await User.findByIdAndUpdate(loginedID, updates, {
+        new: true
       });
-    else return res.send({ message: "This user id is not exist" });
-  } catch (e) {
-    return res.send({ error: e });
-  }
-});
+      if (user)
+        return res.send({ message: 'user was edited successfully', image: user.image })
+      else
+        return res.send({ message: 'This user id is not exist' })
+
+    } catch (e) {
+      return res.send({ error: e })
+    }
+  })
 
 /////////////////////// get all users ////////////////////////////////
-router.get("/", auth, async (req, res) => {
-  const loginedID = req.user._id;
+router.get('/', auth, async (req, res) => {
+  const loginedID = req.user._id
   const user = await User.findById(loginedID);
 
-  console.log(user);
-  return res.send(user);
-});
+  console.log(user)
+  return res.send(user)
+
+})
+
 
 ///////////////////////// edit user by id///////////////////////////////
 
-router.patch("/update", auth, async (req, res) => {
-  const loginedID = req.user._id;
+router.patch('/update', auth, async (req, res) => {
+
+  const loginedID = req.user._id
   let user = await User.findById(loginedID);
 
-  console.log(req.body);
+  console.log(req.body)
+
 
   let updates = {
-    email:
-      req.body.email != "" && req.body.email != null
-        ? req.body.email
-        : user.email,
-    username:
-      req.body.username != "" && req.body.username != null
-        ? req.body.username
-        : user.username,
-    password:
-      req.body.password != "" && req.body.password != null
-        ? req.body.password
-        : user.password,
-    gender:
-      req.body.gender != "" && req.body.gender != null
-        ? req.body.gender
-        : user.gender,
-  };
+    email: (req.body.email != "" && req.body.email != null) ? req.body.email : user.email,
+    username: (req.body.username != "" && req.body.username != null) ? req.body.username : user.username,
+    password: (req.body.password != "" && req.body.password != null) ? req.body.password : user.password,
+    gender: (req.body.gender != "" && req.body.gender != null) ? req.body.gender : user.gender
+
+  }
   user = await User.findByIdAndUpdate(loginedID, updates, {
-    new: true,
+    new: true
   });
   if (user)
-    return res.send({ message: "user was edited successfully", user: user });
-  else return res.send({ message: "This user id is not exist" });
-});
+    return res.send({ message: 'user was edited successfully', user: user })
+  else
+    return res.send({ message: 'This user id is not exist' })
+
+})
 
 /////////////////////// delete product by id//////////////////////
 
-router.delete("/delete", auth, async (req, res) => {
-  const loginedID = req.user._id;
+router.delete('/delete', auth, async (req, res) => {
+  const loginedID = req.user._id
   const user = await User.findById(loginedID);
   // await cloudinary.uploader.destroy(user.cloudinary_id);
-  await User.deleteOne(user);
-  if (user) return res.send({ message: "user deleted successfuly" });
-});
+  await User.deleteOne(user)
+  if (user) return res.send({ message: 'user deleted successfuly' })
+})
 
-//verify account via mail
-router.get("/verify/:id", async function (req, res) {
+//verify account via mail 
+router.get('/verify/:id', async function (req, res) {
+
   const { id } = req.params;
 
   //Checkin if the user exists
@@ -157,12 +149,13 @@ router.get("/verify/:id", async function (req, res) {
 
   //change state if exist to be active
   const activate = {
-    isActive: true,
-  };
+    isActive: true
+  }
   await User.findByIdAndUpdate(id, activate);
 
   res.status(200);
-  res.sendFile("views/activation.html", { root: __dirname });
-});
+  res.sendFile('views/activation.html', { root: __dirname });
+})
 
-module.exports = router;
+
+module.exports = router
