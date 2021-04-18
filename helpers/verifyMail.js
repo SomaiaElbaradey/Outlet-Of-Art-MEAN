779955e6f6d26aa4fa840ed10249246e2f6acd60
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-04-19 01:58:57 +0200

    f

--- helpers/verifyMail.js
+++ helpers/verifyMail.js
@@ -15,21 +15,21 @@ module.exports.sendMail = async function (userMail, username, userId) {
 
         let info = await transporter.sendMail({
             from: {
-                name: 'Art-Real',
+                name: 'outlet-of-art',
                 address: 'brandeldev@gmail.com'
             },
             to: `${userMail}`,
-            subject: `[Art-Real] Welcome to Art-Real ${username}`,
-            text: "Welcome to Art-Real",
+            subject: `[outlet-of-art] Welcome to outlet-of-art ${username}`,
+            text: "Welcome to outlet-of-art",
             html: `
                    <body style="color: white; margin: 0 auto; width: 50%; background-color: black; padding:50px">
-                    <h1>Welcome to Art-Real!</h1>
+                    <h1>Welcome to outlet-of-art!</h1>
                     <div style="color:rgb(18, 102, 95); background-color: white; 
                                     padding: 60px; border-radius: 5px; border: rgb(69, 85, 83) 2px solid;">
-                        <h3>Almost done, ${username}! To complete your Art-Real sign up, we just need to verify your 
+                        <h3>Almost done, ${username}! To complete your outlet-of-art sign up, we just need to verify your 
                             email address: ${userMail}</h3>
                         <div>
-                         <a none;" href="https://art-real.herokuapp.com/api/users/verify/${userId}">
+                         <a none;" href="https://outlet-of-art.herokuapp.com/api/users/verify/${userId}">
                          <button style="height: 30px; background-color: #44c1c1c7; 
                          border: none; border-radius: 5%; margin: 10px; cursor: pointer;"> Verify your mail address</button><br>
                          </a>
